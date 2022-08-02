912f702b790a45ad3f8bdcb4148541cb038d82c4
serhii-sultanov <sshsultanov@gmail.com>
2022-08-02 22:31:32 +0300

    created function findShort

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ const sumToShort = (n) => (n === 1 ? n : n + sumToShort(n - 1));
 
 console.log(sumToShort(4));
 console.log(sumToShort(3));
+
+// 2
+const sentance = "Lorem ipsum dolor sit amet";
+
+function findShort(string) {
+  const shortestWord = string.split(" ").sort((a, b) => a.length - b.length);
+  return shortestWord[0];
+}
+
+console.log(findShort(sentance));
+console.log(findShort("Hello world"));
+console.log(findShort("Hi"));
+console.log(findShort("She is David's sister"));
